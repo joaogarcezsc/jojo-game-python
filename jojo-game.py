c9d1b7fd54151198bd50ff4e19465d4e641d9021
João Manoel <joaomanoell875@outlook.com.br>
2026-07-08 23:19:38 -0300

    Commit inicial: Criaçãp dos Arquios

---
+++ jojo-game.py
@@ -0,0 +1,10 @@
+import os
+
+#----------FALAS----------#
+
+def saudacaoInical():
+    print("\n-----BEM-VINDO AO GAME DE JOJO'S BIZARRES ADVENTURE EM PYTHON !-----\n")
+    print("Nesse jogo você irá controlar o grupo de amigos liderados por Joseph Joestar e Jotaro Kujo em sua jornada para o Egito na caça de Dio, para salvar a vida de Holy Joestar !\n")
+    print("Tecle enter para seguir para as regras do jogo\n")
+
+saudacaoInical()
